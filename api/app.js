618be957a1c734076d120fcc9c34d00f28788c46
MxKenine <quentind.duprey@gmail.com>
2026-09-14 10:00:21 +0200

    corretif pour render v2

--- api/app.js
+++ api/app.js
@@ -20,7 +20,7 @@ const FRONT_URL = process.env.FRONT_URL;
 
 app.use(cors({
   origin: FRONT_URL,
-  credentials: 'include',
+  credentials: true,
 }));
 app.use(cookieParser());
 app.use(express.json());
