3d5cc89f64087d74ae24bd098ef84d966114c85e
MxKenine <quentind.duprey@gmail.com>
2026-09-15 09:02:35 +0200

    l'import judicieux de eye/off

--- client/src/pages/public/Login.jsx
+++ client/src/pages/public/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { ShieldCheck } from "lucide-react";
+import { ShieldCheck, Eye, EyeOff } from "lucide-react";
 
 export default function Login() {
   const [email, setEmail] = useState("");
