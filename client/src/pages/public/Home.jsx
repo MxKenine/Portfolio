2cc591b3aef6cdd715dcdabf0498011d4b0d64a1
MxKenine <quentind.duprey@gmail.com>
2026-09-16 11:29:12 +0200

    maj img v4

--- client/src/pages/public/Home.jsx
+++ client/src/pages/public/Home.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import defaultAvatar from "../assets/default-avatar.jpeg"; 
+import defaultAvatar from "../../assets/default-avatar.jpeg"; 
 
 export default function HomeHero() {
   return (
