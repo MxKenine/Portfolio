92e0cf91c648372aaa2119f3a009c6e1924c5d64
MxKenine <quentind.duprey@gmail.com>
2026-09-16 11:18:34 +0200

    maj img v2

--- client/src/pages/public/Home.jsx
+++ client/src/pages/public/Home.jsx
@@ -7,7 +7,7 @@ export default function HomeHero() {
         <div className="grid md:grid-cols-2 gap-10 md:gap-12 items-center">
           <div className="flex justify-center md:justify-end order-1 md:order-2">
             <img
-              src="../../../api/uploads/default-avatar.jpeg"
+              src="/assets/default-avatar.jpeg"
               alt="Photo de profil"
               className="w-48 h-48 md:w-72 md:h-72 rounded-full object-cover shadow-lg"
             />
